76720af9088680ed50d0e248817ae917b61d86cd
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2020-05-15 20:09:22 +0300

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,7 +4,7 @@ import AppReducer from './AppReducer';
 // Initial state
 const initialState = {
 	transactions: [
-		{ id: 1, text: 'Flower', amount: -20 },
+		{ id: 1, text: 'New Rochelle Balls', amount: -50 },
 		{ id: 2, text: 'Salary', amount: 300 },
 		{ id: 3, text: 'Book', amount: -10 }
 	]
