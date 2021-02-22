04f6d05d0b7823ffe3eea0b3898b6728646c8658
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2021-02-22 13:55:26 +0300

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,18 +1,16 @@
-// A reducer is a way to change your state and send it down to your component/application
-export default (state, action) => {
-	switch (action.type) {
-		case 'DELETE_TRANSACTION':
-			return {
-				// we cant just change it, we have to create a new state and send it down.. we could use a spread op
-				...state,
-				transactions: state.transactions.filter((transaction) => transaction.id !== action.payload)
-			};
-		case 'ADD_TRANSACTION':
-			return {
-				...state,
-				transactions: [ action.payload, ...state.transactions ]
-			};
-		default:
-			return state;
-	}
-};
+export const AppReducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter((transaction)=>transaction.id !== action.payload )
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload],
+            };
+        default:
+            return state
+    }
+}
