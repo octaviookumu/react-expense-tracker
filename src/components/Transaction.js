40e1970c37e979c74040001858f9b6ad0ca3c4a1
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2021-02-22 13:57:01 +0300

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,21 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
-import { GlobalContext } from '../context/GlobalState';
+const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
 
-export const Transaction = ({ transaction }) => {
-	const { deleteTransaction } = useContext(GlobalContext);
+    const sign = transaction.amount < 0 ? "-" : "+";
 
-	const sign = transaction.amount < 0 ? '-' : '+';
-
-	return (
-		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-			{transaction.text}{' '}
-			<span>
-				{sign}${Math.abs(transaction.amount)}
-			</span>
-			<button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">
-				x
-			</button>
-		</li>
-	);
+    return (
+        <li className={sign === "-" ? "minus" : "plus"}>
+            {transaction.text}{" "}
+            <span>
+                {sign}${Math.abs(transaction.amount)}
+            </span>
+            <button
+                className="delete-btn"
+                onClick={() => deleteTransaction(transaction.id)}
+            >
+                x
+            </button>
+        </li>
+    );
 };
+
+export default Transaction;
